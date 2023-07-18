17001f990b9f660727cb81443b2bb92ca7f3415a
Dakalo <dakalodakie7@gmail.com>
2023-07-18 21:37:24 +0200

    added init backend

---
+++ Backend/src/main/java/com/activelearningsync/ActiveLearningSync/ActiveLearningSyncApplication.java
@@ -0,0 +1,13 @@
+package com.activelearningsync.ActiveLearningSync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveLearningSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActiveLearningSyncApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/activelearningsync/ActiveLearningSync/ActiveLearningSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.activelearningsync.ActiveLearningSync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveLearningSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
